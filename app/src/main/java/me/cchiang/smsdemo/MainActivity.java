f4349be949d0be6dc0bcd3fbdb14cc09a98dab88
Christian <christiandcf@gmail.com>
2017-10-21 14:14:37 -0500

    committing

--- app/src/main/java/me/cchiang/smsdemo/MainActivity.java
+++ app/src/main/java/me/cchiang/smsdemo/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         txt1 = (TextView)findViewById(R.id.txt1);
         conList = new ArrayList<>();
         requestPerms();
