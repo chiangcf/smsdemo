d1cbbc3a29603726ed41e756724672016239d330
Christian <christiandcf@gmail.com>
2017-10-20 23:19:31 -0500

    saving

--- app/src/main/java/me/cchiang/smsdemo/MainActivity.java
+++ app/src/main/java/me/cchiang/smsdemo/MainActivity.java
@@ -2,7 +2,9 @@ package me.cchiang.smsdemo;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -26,16 +28,16 @@ public class MainActivity extends AppCompatActivity {
         mBtn1 = (Button) findViewById(R.id.btn1);
 
         // Request permissions or request them
-        if(checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+//        if(checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
             mBtn1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     sendSMS();
                 }
             });
-        }else{
+//        }else{
             requestPerms();
-        }
+//        }
 
 
 
@@ -47,15 +49,20 @@ public class MainActivity extends AppCompatActivity {
         String number = "5158670942";
         String message = "Hello";
 
-        SmsManager manager  =SmsManager.getDefault();
+        SmsManager manager = SmsManager.getDefault();
+        String SENT = "SENT";
         manager.sendTextMessage(number, null, message, null, null);
-        Toast.makeText(getApplicationContext(), "send succesfully", Toast.LENGTH_LONG).show();
+        manager.sendTextMessage(number, null, "no y que", null, null);
+
+        Toast.makeText(getApplicationContext(), "send successfully", Toast.LENGTH_SHORT).show();
+
+
     }
 
     protected void requestPerms(){
-        requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
-        requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
-        requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS}, 1);
+        requestPermissions(new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//        requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
+//        requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS}, 1);
 
     }
 
